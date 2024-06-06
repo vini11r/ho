debc7a42b7d09bfa0eae7201a09c0363fe8c608c
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-06-06 21:39:58 +0300

    Новое приложение blog

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.url", namespace="catalog")),
+    path("", include("blog.url", namespace="blog")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
