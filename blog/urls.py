48daf3aa58f01ffb4024477e5b354713f769303a
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-06-09 21:24:14 +0300

    Новое приложение blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView, BlogListView, BlogDetailView
+from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path("create/", BlogCreateView.as_view(), name="blog_create"),
     path("", BlogListView.as_view(), name="blog_list"),
-    path("detail/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),]
+    path("detail/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("update/<int:pk>/", BlogUpdateView.as_view(), name="blog_update"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.template.defaultfilters import slugify
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from blog.models import Blog
 
 
@@ -17,4 +17,20 @@ class BlogCreateView(CreateView):
     model = Blog
     fields = '__all__'
     success_url = reverse_lazy('blog:blog_list')
-    template_name = 'blog/blog_create.html'
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = '__all__'
+    # success_url = reverse_lazy('blog:blog_update')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_record = form.save()
+            new_record.slug = slugify(new_record.title)
+            new_record.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:blog_update', args=[self.kwargs.get('pk')])
