f8f8a13d01de2c1a5abfaa43d3b1b4861fca6153
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-06-10 12:24:59 +0300

    реализовал list

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView
+from blog.views import BlogCreateView, BlogListView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path("create/", BlogCreateView.as_view(), name="create"),
+    path("", BlogListView.as_view(), name="list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from blog.models import Blog
 
@@ -7,4 +7,14 @@ from blog.models import Blog
 class BlogCreateView(CreateView):
     model = Blog
     fields = ["title", "slug", "content", "preview", "created_at", "published", "count_views"]
-    success_url = reverse_lazy('catalog:product_list')
+    success_url = reverse_lazy('blog:list')
+
+
+class BlogListView(ListView):
+    model = Blog
+    context_object_name = 'objects_list'
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(published=True)
+        return queryset
